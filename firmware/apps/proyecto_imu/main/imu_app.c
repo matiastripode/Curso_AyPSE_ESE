38e2d1a2b663776eb761f1c1c11552a7e62a2930
Matias Tripode <matiastripode@gmail.com>
2026-07-24 11:43:03 -0400

    [app] integra ble_imu en la aplicacion

--- firmware/apps/proyecto_imu/main/imu_app.c
+++ firmware/apps/proyecto_imu/main/imu_app.c
@@ -26,14 +26,29 @@
 /*==================[inclusions]=============================================*/
 #include <stdio.h>
 #include <stdint.h>
+#include "nvs_flash.h"
+#include "ble_imu.h"
+#include "esp_log.h"
+
 /*==================[macros and definitions]=================================*/
 
 /*==================[internal data definition]===============================*/
-
+static const char *TAG = "IMU_APP";
 /*==================[internal functions declaration]=========================*/
 
 /*==================[external functions definition]==========================*/
 void app_main(void){
-	printf("Hello world!\n");
+	esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ble_imu_error_t ble_result = BLEIMUInit();
+
+    if (ble_result != BLE_IMU_OK) {
+        ESP_LOGE(TAG,  "Main App Failed");
+    }
 }
 /*==================[end of file]============================================*/
